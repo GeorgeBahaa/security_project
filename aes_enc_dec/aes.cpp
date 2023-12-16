ec09430be00feed8357c8d764964c4e44149f815
George Essam <104697945+George-essam@users.noreply.github.com>
2023-12-16 16:47:21 +0200

    aes.cpp

--- aes_enc_dec/aes.cpp
+++ aes_enc_dec/aes.cpp
@@ -1,31 +1,54 @@
 #include "aes.h"
 
-using namespace std;
-
 namespace project {
     namespace aes {
-void handleErrors() {
-    std::cerr << "Error occurred." << std::endl;
-    exit(EXIT_FAILURE);
-}
+        void handleErrors() {
+            std::cerr << "Error occurred." << std::endl;
+            exit(EXIT_FAILURE);
+        }
 
-void encrypt(const char *plaintext, const char *key, unsigned char *ciphertext) {
-    AES_KEY encryptKey;
-    if (AES_set_encrypt_key(reinterpret_cast<const unsigned char *>(key), 128, &encryptKey) < 0) {
-        handleErrors();
+        // Padding function
+        void addPadding(std::string& data) {
+            char padding = AES_BLOCK_SIZE - data.size() % AES_BLOCK_SIZE;
+            data.append(padding, padding);
+        }
+
+        void encrypt(const std::string& plaintext, const std::string& key, std::vector<unsigned char>& ciphertext) {
+            AES_KEY encryptKey;
+            if (AES_set_encrypt_key(reinterpret_cast<const unsigned char*>(key.c_str()), 128, &encryptKey) < 0) {
+                handleErrors();
+            }
+
+            std::string paddedPlaintext = plaintext;
+            addPadding(paddedPlaintext);
+
+            // Determine the size of the ciphertext and resize the vector
+            size_t ciphertextSize = paddedPlaintext.size();
+            ciphertext.resize(ciphertextSize);
+
+            // Encrypt block by block
+            for (size_t i = 0; i < paddedPlaintext.size(); i += AES_BLOCK_SIZE) {
+                AES_encrypt(reinterpret_cast<const unsigned char*>(&paddedPlaintext[i]), &ciphertext[i], &encryptKey);
+            }
+        }
+
+        void decrypt(const std::vector<unsigned char>& ciphertext, const std::string& key, std::vector<char>& decryptedtext) {
+            AES_KEY decryptKey;
+            if (AES_set_decrypt_key(reinterpret_cast<const unsigned char*>(key.c_str()), 128, &decryptKey) < 0) {
+                handleErrors();
+            }
+
+            // Resize the decryptedtext vector to hold the decrypted text
+            decryptedtext.resize(ciphertext.size());
+
+            // Decrypt block by block
+            for (size_t i = 0; i < ciphertext.size(); i += AES_BLOCK_SIZE) {
+                AES_decrypt(&ciphertext[i], reinterpret_cast<unsigned char*>(&decryptedtext[i]), &decryptKey);
+            }
+
+            // Remove padding
+            size_t paddingSize = decryptedtext.back();
+            decryptedtext.resize(decryptedtext.size() - paddingSize);
+        }
     }
-
-    AES_encrypt(reinterpret_cast<const unsigned char *>(plaintext), ciphertext, &encryptKey);
-}
-
-void decrypt(const unsigned char *ciphertext, const char *key, char *decryptedtext) {
-    AES_KEY decryptKey;
-    if (AES_set_decrypt_key(reinterpret_cast<const unsigned char *>(key), 128, &decryptKey) < 0) {
-        handleErrors();
-    }
-
-    AES_decrypt(ciphertext, reinterpret_cast<unsigned char *>(decryptedtext), &decryptKey);
-}
-
-}
 }
